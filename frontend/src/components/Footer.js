70ae87974f70bafefc3317c1feda10de7e830353
Bawanthathilan <bawantharathnayaka@gmail.com>
2021-12-02 23:40:42 +0530

    added home screen , product screen and setup react router

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+const Footer = () => {
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Col className='text-center py-3'>Copyright &copy; Eshop</Col>
+        </Row>
+      </Container>
+    </div>
+  )
+}
+
+export default Footer
