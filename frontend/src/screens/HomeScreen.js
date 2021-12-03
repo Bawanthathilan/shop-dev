e067d8e16c59b8f66a7ea20dcc1db6bc36d1ef0c
Bawanthathilan <bawantharathnayaka@gmail.com>
2021-12-03 23:35:48 +0530

    added loading spinner

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,29 +1,34 @@
-import React, { useState , useEffect} from 'react'
-import { Row, Col } from 'react-bootstrap'
+import React, { useState, useEffect } from 'react'
+import { Row, Col, Spinner } from 'react-bootstrap'
 import axios from 'axios'
 import Product from '../components/Product.js'
 
 const HomeScreen = () => {
   const [products, setProducts] = useState([])
+  const [isloading, setLoading] = useState(true)
 
-  useEffect(()=>{
-   const fetchProducts = async () =>{
-     const {data} = await axios.get('/api/products')
-
-     setProducts(data)
-   }
-   fetchProducts()
-  },[])
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products')
+      setLoading(false)
+      setProducts(data)
+    }
+    fetchProducts()
+  }, [])
   return (
     <>
       <h1>Latest products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {isloading ? (
+        <Spinner />
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   )
 }
