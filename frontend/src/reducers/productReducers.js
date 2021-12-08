aa51149f0b01c3ee64656a802d18b9fadba84a37
Bawanthathilan <bawantharathnayaka@gmail.com>
2021-12-08 11:52:00 +0530

    product details reducer and action

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,4 +1,11 @@
-import {PRODUCT_LIST_FAIL ,PRODUCT_LIST_REQUEST , PRODUCT_LIST_SUCCESS} from '../constants/productConstants'
+import {
+  PRODUCT_LIST_FAIL,
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_DETAIL_SUCCESS,
+  PRODUCT_DETAIL_REQUEST,
+  PRODUCT_DETAIL_FAIL,
+} from '../constants/productConstants'
 
 export const productListReducer = (state = { products: [] }, action) => {
   switch (action.type) {
@@ -8,6 +15,22 @@ export const productListReducer = (state = { products: [] }, action) => {
       return { loading: false, products: action.payload }
     case PRODUCT_LIST_FAIL:
       return { loading: false, error: action.payload }
+    default:
+      return state
+  }
+}
+
+export const productDetailsReducer = (
+  state = { product: { reviews: [] } },
+  action
+) => {
+  switch (action.type) {
+    case PRODUCT_DETAIL_REQUEST:
+      return { loading: true, ...state }
+    case PRODUCT_DETAIL_SUCCESS:
+      return { loading: false, product: action.payload }
+    case PRODUCT_LIST_FAIL:
+      return { loading: false, product: action.payload }
     default:
       return state
   }
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,6 +1,9 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
-import axios from 'axios'
+
+import { listProductDetails } from '../actions/productActions'
+
 import {
   Row,
   Col,
@@ -9,75 +12,79 @@ import {
   Card,
   Button,
   ListGroupItem,
+  Spinner,
 } from 'react-bootstrap'
 import Rating from '../components/Rating'
 
 const ProductScreen = ({ match }) => {
-  const [product, setProduct] = useState({})
+  const dispatch = useDispatch()
+  const productDetails = useSelector((state) => state.productDetails)
+  const { loading, error, product } = productDetails
 
   useEffect(() => {
-    const fetchProduct = async () => {
-      const { data } = await axios.get(`/api/products/${match.params.id}`)
-
-      setProduct(data)
-    }
-    fetchProduct()
-  }, [match])
+    dispatch(listProductDetails(match.params.id))
+  }, [dispatch, match])
 
   return (
     <>
       <Link className='btn btn-light my-3' to='/'>
         Go Back
       </Link>
-      <Row>
-        <Col md={6}>
-          <Image src={product.image} alt={product.name} fluid />
-        </Col>
-        <Col md={3}>
-          <ListGroup variant='flush'>
-            <ListGroupItem>
-              <h2>{product.name}</h2>
-            </ListGroupItem>
-            <ListGroupItem>
-              <Rating
-                value={product.rating}
-                text={`${product.numReviews} reviews`}
-              />
-            </ListGroupItem>
-            <ListGroupItem>Price:{product.price}</ListGroupItem>
-            <ListGroupItem>description:{product.description}</ListGroupItem>
-          </ListGroup>
-        </Col>
-        <Col md={3}>
-          <Card>
+      {loading ? (
+        <Spinner />
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          <Col md={6}>
+            <Image src={product.image} alt={product.name} fluid />
+          </Col>
+          <Col md={3}>
             <ListGroup variant='flush'>
               <ListGroupItem>
-                <Row>
-                  <Col>Price :</Col>
-                  <Col>{product.price}</Col>
-                </Row>
+                <h2>{product.name}</h2>
               </ListGroupItem>
               <ListGroupItem>
-                <Row>
-                  <Col>Status :</Col>
-                  <Col>
-                    {product.countInStock > 0 ? 'In Stock' : 'Out of stock'}
-                  </Col>
-                </Row>
+                <Rating
+                  value={product.rating}
+                  text={`${product.numReviews} reviews`}
+                />
               </ListGroupItem>
-              <ListGroupItem>
-                <Button
-                  className='btn-block'
-                  type='button'
-                  disabled={product.countInStock === 0}
-                >
-                  Add to cart
-                </Button>
-              </ListGroupItem>
+              <ListGroupItem>Price:{product.price}</ListGroupItem>
+              <ListGroupItem>description:{product.description}</ListGroupItem>
             </ListGroup>
-          </Card>
-        </Col>
-      </Row>
+          </Col>
+          <Col md={3}>
+            <Card>
+              <ListGroup variant='flush'>
+                <ListGroupItem>
+                  <Row>
+                    <Col>Price :</Col>
+                    <Col>{product.price}</Col>
+                  </Row>
+                </ListGroupItem>
+                <ListGroupItem>
+                  <Row>
+                    <Col>Status :</Col>
+                    <Col>
+                      {product.countInStock > 0 ? 'In Stock' : 'Out of stock'}
+                    </Col>
+                  </Row>
+                </ListGroupItem>
+                <ListGroupItem>
+                  <Button
+                    className='btn-block'
+                    type='button'
+                    disabled={product.countInStock === 0}
+                  >
+                    Add to cart
+                  </Button>
+                </ListGroupItem>
+              </ListGroup>
+            </Card>
+          </Col>
+        </Row>
+      )}
     </>
   )
 }
