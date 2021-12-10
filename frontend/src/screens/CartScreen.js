3091f2fbf960f078e64d687c521635b77f5a944a
Bawanthathilan <bawantharathnayaka@gmail.com>
2021-12-10 17:27:13 +0530

    remove cart handler

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -13,7 +13,7 @@ import {
   ListGroupItem,
 } from 'react-bootstrap'
 
-import { addToCart } from '../actions/cartActions'
+import { addToCart, removeFromCart } from '../actions/cartActions'
 
 const CartScreen = ({ match, location, history }) => {
   const productId = match.params.id
@@ -31,7 +31,7 @@ const CartScreen = ({ match, location, history }) => {
   }, [dispatch, productId, qty])
 
   const removeFromCartHandler = (id) => {
-    console.log('remove')
+    dispatch(removeFromCart(id))
   }
 
   const checkoutHandler = () => {
@@ -78,7 +78,7 @@ const CartScreen = ({ match, location, history }) => {
                     <Button
                       type='button'
                       variant='light'
-                      onCLick={() => removeFromCartHandler(item.product)}
+                      onClick={() => removeFromCartHandler(item.product)}
                     >
                       <i className='fas fa-trash'></i>
                     </Button>
